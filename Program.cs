2223f5e417cfbf3ca396cd5a6e41b85e91cd4293
navabhi22 <navabhi22@gmail.com>
2022-03-08 21:35:34 +0530

    Abhinav Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,18 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program");
+            Console.WriteLine("Enter First Name (First letter capital and min 3 charater)");
+            string FirstName = Console.ReadLine();
+            Name name = new Name();
+            bool match = name.FirstName(FirstName);
+            if (match == true)
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Enter a Valid First Name");
+            }
         }
     }
 }
