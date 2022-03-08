a8c26c0532c0f1324c0986f90cd2bc3d5243ef96
navabhi22 <navabhi22@gmail.com>
2022-03-08 21:44:24 +0530

    Abhinav Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -6,18 +6,22 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration Program");
             Console.WriteLine("Enter First Name (First letter capital and min 3 charater)");
             string FirstName = Console.ReadLine();
-            Name name = new Name();
-            bool match = name.FirstName(FirstName);
-            if (match == true)
-            {
-                Console.WriteLine("Valid First Name");
-            }
-            else
+            Name first_name = new Name();
+            bool match = first_name.ValidName(FirstName);
+            if (match != true)
             {
                 Console.WriteLine("Enter a Valid First Name");
+            }
+
+            Console.WriteLine("Enter Last Name (First letter capital and min 3 charater)");
+            string LastName = Console.ReadLine();
+            Name last_name = new Name();
+            bool match2 = last_name.ValidName(LastName);
+            if (match != true)
+            {
+                Console.WriteLine("Enter a valid Second Name");
             }
         }
     }
