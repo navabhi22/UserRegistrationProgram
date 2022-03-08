bffa1f931bb0fddbce58bb67f8acd487ac273941
navabhi22 <navabhi22@gmail.com>
2022-03-09 00:52:25 +0530

    Abhinav Added Password.cs

---
+++ Password.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Password
+    {
+        String passRegex = "^[a-zA-Z0-9]*.{8,}$";
+        public bool Rule1(string password)
+        {
+            return Regex.IsMatch(password, passRegex);
+        }
+    }
+}
