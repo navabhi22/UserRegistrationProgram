fedc71cb41cd75dbf2e1fbbc7df58a2f796d9f55
navabhi22 <navabhi22@gmail.com>
2022-03-08 21:33:41 +0530

    Abhinav Added Name.cs

---
+++ Name.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class Name
+    {/// <summary>
+    /// Validating FirstName
+    /// </summary>
+        public static string Pattern = "^[A-Z][a-z]{2}";
+        public bool FirstName(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, Pattern);
+        }
+    }
+}
