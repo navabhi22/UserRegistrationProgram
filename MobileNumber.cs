cde79f1c9d776075b728e21cb8cc2bb7aa8ade8b
navabhi22 <navabhi22@gmail.com>
2022-03-09 00:38:28 +0530

    Abhinav Added MobileNumber.cs

---
+++ MobileNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class MobileNumber
+    {
+        public class PatternMobileNum
+        {
+            public static string Regex_Pattern = "^[+][9][1][' ']*[7-9][0-9]{9}$";
+            public bool ValidateNumber(string number)
+            {
+                return Regex.IsMatch(number, Regex_Pattern);
+            }
+        }
+    }
+}
